f62b3ba200ce9dec62d2e206a8083c4c606556a1
Ossiggy <mrossig201@gmail.com>
2018-04-18 19:58:45 -0400

    have continued to develop state altering and rendering functions

--- app.js
+++ app.js
@@ -7,29 +7,42 @@ const template = Handlebars.compile(source);
 
 // set state for DOM rendering
 const state = {
-  user: null,
+  user: {},
   budget:{
     _parent:null,
     income:0,
     totalSpent:0,
     categories: []
   },
-  isEditing:false
+  isEditing:false,
+  menuOpen: false
 };
 
 
 
 //state altering functions
 
-function addCategoryToState(category){
+function setRoute(state, route){
+  state.route = route;
+}
+
+function addCategoryToState(state, category){
   let categories = state.budget.categories;
   categories.push(category)
   renderCategories(categories);
 }
 
+function updateUser(state, object){
+  let {userId, authToken, username} = object;
+  state.user.userId = userId;
+  state.user.authToken = authToken;
+  state.user.username = username;
+
+}
+
 //check functions
 
-function checkForExistingCategory(newCategory){
+function checkForExistingCategory(state, newCategory){
   let counter = 0;
   state.budget.categories.forEach(function(category){
     if(category.name===newCategory.name){    
@@ -53,10 +66,28 @@ function checkForExistingCategory(newCategory){
 
 //Event listeners
 
+$(document).ready(renderStartPage);
+$('header').on('click', 'sign-in-submit', extractUserData);
 $('#new-category-submit').on('click', createNewCategory);
 
 
 //Event Handlers
+
+function renderStartPage(event){
+  event.preventDefault();
+  setRoute(state, 'landing-page');
+  renderApp(state, PAGE_ELEMENTS);
+}
+
+function extractUserData(event){
+  event.preventDefault();
+  const formData = {};
+  $('#sign-in input').each(function(){
+    let {name, value} = this;
+    formData[name]=value;
+  });
+  userLogin(formData);
+};
 
 function createNewCategory(event){
   event.preventDefault();
@@ -65,14 +96,69 @@ function createNewCategory(event){
   newCategory.name = $('#category-name').val();
   newCategory.amount = $('#category-amount').val();
   $('#budget-form')[0].reset()
-  checkForExistingCategory(newCategory);
+  checkForExistingCategory(state, newCategory);
+}
+
+//auth functions
+
+function userLogin(userData){
+
+  const loginURL = '/api/auth/login';
+  
+    const {username, password} = formData;
+  
+    function setHeader(req){
+      const encodedString = btoa(`${username}:${password}`);
+      req.setRequestHeader('Authorization', 'Basic ' + encodedString);
+    }
+  
+    function handleSuccess(res){
+
+      const userObject = {
+        userId: res.id,
+        authToken:res.authToken
+      }
+  
+      $.get('api/users/'+userObject.userId)
+        .then(res => {
+          userObject.username=res.username;
+        });
+
+      updateUser(state, userObject);
+      setRoute(state, 'budget-page');
+      renderApp(state, PAGE_ELEMENTS);
+
+    }
+  
+    const infoSettings = {
+      url: loginURL,
+      type: 'POST',
+      beforeSend: setHeader,
+      data: formData,
+      success: handleSuccess,
+      error: function(err){
+        console.log(err);
+      }
+    };
+
+    $.ajax(infoSettings);
 }
 
 //rendering functions
 
-function renderPage(state){
-  return
+const PAGE_ELEMENTS = {
+  'landing-page': $('.landing-page'),
+  'budget-page': $('.budget-page')
 }
+
+function renderApp(state, elements){
+  Object.keys(elements).forEach(function(route){
+    elements[route].hide()
+  })
+  elements[state.route].show()
+  if(state.route==='.landing-page'){
+    renderLandingPage(state, element.find('.landing-page'));
+};
 
 function renderCategories(categories){
   $('.budget-container').html('');
